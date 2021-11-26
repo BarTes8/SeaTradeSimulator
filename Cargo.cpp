2c9a726b7196025939a96e62e4b2407199ff2196
BarTes8 <barbara.tessema@gmail.com>
2021-11-26 20:26:03 +0100

    add getters in Cargo class

--- Cargo.cpp
+++ Cargo.cpp
@@ -7,6 +7,18 @@ Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice)
     , amount_(amount)
     , basePrice_(basePrice)
 {}
+
+std::string Cargo::getName() const {
+    return name_;
+}
+
+size_t Cargo::getAmount() const {
+    return amount_;
+}
+
+size_t Cargo::getBasePrice() const {
+    return basePrice_;
+}
     
 Cargo& Cargo::operator+=(const size_t amount) {
     if (amount_ + amount <= std::numeric_limits<size_t>::max()) {
--- Cargo.hpp
+++ Cargo.hpp
@@ -7,6 +7,10 @@ class Cargo {
 public:
     Cargo(const std::string& name, size_t amount, size_t basePrice);
 
+    std::string getName() const;
+    size_t getAmount() const;
+    size_t getBasePrice() const;
+
     Cargo& operator+=(const size_t amount);
     Cargo& operator-=(const size_t amount);
     bool operator ==(const Cargo& cargo) const;
