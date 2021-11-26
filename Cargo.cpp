2626c3b230b3f265d3aebbb1d14fb2c9f6cc529a
BarTes8 <barbara.tessema@gmail.com>
2021-11-26 19:55:05 +0100

    add constructor in Cargo class

--- Cargo.cpp
+++ Cargo.cpp
@@ -1,6 +1,12 @@
 #include "Cargo.hpp"
 
 #include <limits>
+
+Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice) 
+    : name_(name)
+    , amount_(amount)
+    , basePrice_(basePrice)
+{}
     
 Cargo& Cargo::operator+=(const size_t amount) {
     if (amount_ + amount <= std::numeric_limits<size_t>::max()) {
--- Cargo.hpp
+++ Cargo.hpp
@@ -5,6 +5,8 @@
 
 class Cargo {
 public:
+    Cargo(const std::string& name, size_t amount, size_t basePrice);
+
     Cargo& operator+=(const size_t amount);
     Cargo& operator-=(const size_t amount);
     bool operator ==(const Cargo& cargo) const;
